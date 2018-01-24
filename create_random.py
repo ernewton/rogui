b29b71fd4d5bc8a712f33a64c89144b74b19ac02
Elisabeth Newton <ernewton@mit.edu>
2018-01-24 12:15:45 -0500

    specific lightcurves to test code

--- create_random.py
+++ create_random.py
@@ -1,8 +1,41 @@
 import numpy as np
 rand = np.random.RandomState(42)
 t = 1000 * rand.rand(300)
-y = 0.005*np.sin(2 * np.pi * t/50.) + 0.001 * rand.randn(300)
-dy = np.random.normal(0.001)
+y = 0.005*np.sin(2 * np.pi * t/50.) + 0.002 * rand.randn(len(t))
+dy = 0.001 + 0.002* rand.rand(len(t))
+i = range(300)
 
-for i, ti, yi, dyi in enumerate(*zip(t,y,dy)):
-    
+from astropy.io import ascii
+from astropy.table import Table
+data = Table([i, t, y, dy], names=['i', 'time', 'mag', 'error'])
+ascii.write(data, 'example/random1.dat')
+
+
+rand = np.random.RandomState(42)
+t = 300 * rand.rand(300)
+y = 0.005*np.sin(2 * np.pi * t/2.) + 0.002 * rand.randn(len(t))
+dy = 0.001 + 0.002* rand.rand(len(t))
+i = range(300)
+
+data = Table([i, t, y, dy], names=['i', 'time', 'mag', 'error'])
+ascii.write(data, 'example/random2.dat')
+
+
+rand = np.random.RandomState(42)
+t = 300 * rand.rand(300)
+y = 0.002*np.sin(2 * np.pi * t/2.) + 0.002 * rand.randn(len(t))
+dy = 0.001 + 0.002* rand.rand(len(t))
+i = range(300)
+
+data = Table([i, t, y, dy], names=['i', 'time', 'mag', 'error'])
+ascii.write(data, 'example/random3.dat')
+
+
+rand = np.random.RandomState(42)
+t = 1000 * rand.rand(300)
+y = 0.005*np.sin(2 * np.pi * t/120.) + 0.002 * rand.randn(len(t))
+dy = 0.001 + 0.002* rand.rand(len(t))
+i = range(300)
+
+data = Table([i, t, y, dy], names=['i', 'time', 'mag', 'error'])
+ascii.write(data, 'example/random4.dat')
